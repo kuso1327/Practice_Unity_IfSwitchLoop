1203d47194bfcf58c75445a9191281eaefd8c98b
kuso1327 <63327433+kuso1327@users.noreply.github.com>
2020-05-12 22:52:44 +0800

    IfSwitchLoop

---
+++ Unity_IfSwitchLoop/Assets/Hp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+
+public class Hp : MonoBehaviour
+{
+    [Header("血量"),Range(0,100)]
+    
+    private int _hp;
+    public int hp { get => _hp; set => _hp = value; }
+    
+    
+
+  
+   
+    
+
+    
+
+
+
+}
---
+++ Unity_IfSwitchLoop/Assets/Method.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Method : MonoBehaviour
+{
+    public Hp _hp;
+    public Text result;
+
+    private void Start()
+    {
+        _hp.hp = 100;
+    }
+
+    public void Update()
+    {
+        if (_hp.hp >= 70)
+        {
+            result.text = "安全";
+            print("安全");
+        }
+        else if (_hp.hp >= 30)
+        {
+            result.text = "警告";
+            print("警告");
+        }
+        else
+        {
+            result.text = "危險";
+            print("危險");
+        }
+    }
+}
